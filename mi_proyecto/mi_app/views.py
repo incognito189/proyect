ff9506e4f2849602ddb01cfb764895d4baf4dd5f
incognito189 <ganonymous407@gmail.com>
2025-07-06 21:59:02 -0400

    Update views.py

--- mi_proyecto/mi_app/views.py
+++ mi_proyecto/mi_app/views.py
@@ -1,3 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def inicio(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponse(f'Bienvenido, {user.username}!')
+        else:
+            return render(request, 'index.html', {'error': 'Usuario o contraseña incorrectos'})
+
+    return render(request, 'index.html')
+
+
+
 from django.shortcuts import render
 
 # Create your views here.
