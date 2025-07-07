f976c4989be62ed458549bc829b8fd73da4f4595
incognito189 <ganonymous407@gmail.com>
2025-07-07 14:01:29 -0400

    Update views.py

--- mi_proyecto/mi_app/views.py
+++ mi_proyecto/mi_app/views.py
@@ -1,22 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-def login_view(request):
-    error = None
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            error = "Usuario o contraseña incorrectos"
-
-    return render(request, 'login.html', {'error': error})
-
-@login_required
-def home(request):
-    return render(request, 'home.html', {'user': request.user})
+def index(request):
+    return HttpResponse("Hola desde mi_app")
