cdbba4228c065d267e26f3b5ea6900f88276673e
incognito189 <ganonymous407@gmail.com>
2025-07-06 23:27:37 -0400

    Update urls.py

--- mi_proyecto/mi_app/urls.py
+++ mi_proyecto/mi_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),  # Página de login (raíz)
-    path('home/', views.home, name='home'),    # Página principal después del login
+    path('', views.login_view, name='login'),  # Login en la raíz
+    path('home/', views.home, name='home'),    # Página protegida después de login
 ]
