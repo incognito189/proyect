1114d471a6c1bd2b52372373deea135ee10d8ebf
incognito189 <ganonymous407@gmail.com>
2025-07-06 23:26:28 -0400

    Update settings.py

--- mi_proyecto/mi_proyecto/settings.py
+++ mi_proyecto/mi_proyecto/settings.py
@@ -137,3 +137,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Página donde redirige si NO estás logueado
+LOGIN_URL = '/'
+
+# Página donde redirige después del login exitoso
+LOGIN_REDIRECT_URL = '/home/'
+
